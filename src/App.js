468123d293238686c2f414541b3b7dbdf959b199
Shubham SUnny <shubhamsahaninitkkr@gmail.com>
2020-08-19 20:56:28 +0530

    boiler plate code

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from './components/navbar';
+import ProductList from './components/productList';
+import Default from './components/Default';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={ProductList} />
+          <Route component={Default} />
+        </Switch>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
