05557f0781e0e7b90a2fa3b9a21fd4ad6363f8d8
Shubham SUnny <shubhamsahaninitkkr@gmail.com>
2020-08-20 15:49:53 +0530

    content added

--- src/components/college.js
+++ src/components/college.js
@@ -25,10 +25,33 @@ export default class Product extends Component {
     return items;
   };
 
+  renderRatings = (ratings) => {
+    let items = [1, 2, 3, 4, 5].map((star, idx) => {
+      return ratings >= star ? (
+        <i key={idx} className='fas fa-star'></i>
+      ) : (
+        <i key={idx} className='far fa-star'></i>
+      );
+    });
+    return items;
+  };
+
+  renderNearestPlaces = (places) => {
+    let items = places.map((place, idx) => {
+      return (
+        <span key={idx} className={`mr-2 ${idx !== 0 ? 'text-muted' : ''}`}>
+          {place} {places.length - 1 !== idx && '|'}
+        </span>
+      );
+    });
+    return items;
+  };
+
   render() {
     const {
       college_name,
       famous_nearest_places,
+      nearest_place,
       promoted,
       rating,
       rating_remarks,
@@ -87,10 +110,40 @@ export default class Product extends Component {
           </div>
 
           <div className='card-body p-3'>
-            <h5 className='card-title text-truncate'>{college_name}</h5>
-            <p className='card-text'>
-              {famous_nearest_places} | {}{' '}
-            </p>
+            <div className='row'>
+              <div className='col-md-8'>
+                <h5 className='card-title text-truncate'>
+                  {college_name}
+                  <span className='ml-3'>{this.renderRatings(rating)}</span>
+                </h5>
+                <p className='card-text'>
+                  {this.renderNearestPlaces(nearest_place)}
+                </p>
+                <p>
+                  <span className='text-success'>93% Match : </span>
+                  {famous_nearest_places}
+                </p>
+                <span className='card-text offertxt'>{offertext}</span>
+              </div>
+
+              <div className='col-md-4'>
+                <span className='card-text'>
+                  <i className='fas fa-rupee-sign'></i>{' '}
+                  <del>{original_fees}</del>
+                  <i
+                    className='fas fa-tag text-danger ml-1'
+                    style={{ transform: 'rotate(-45deg)' }}
+                  ></i>
+                  <span className='text-danger'>{discount}%</span>
+                </span>
+                <br />
+                <span className='card-text text-danger'>
+                  <i className='fas fa-rupee-sign'></i> {discounted_fees} <br />{' '}
+                  {fees_cycle}{' '}
+                </span>
+                <p className='card-text text-success'>{amenties}</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
