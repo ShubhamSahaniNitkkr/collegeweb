affc871639efe4daf7c7e0191c99d455b83cf290
Shubham SUnny <shubhamsahaninitkkr@gmail.com>
2020-08-20 12:28:45 +0530

    css

--- src/components/college.js
+++ src/components/college.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { ProductConsumer } from '../context';
 import PropTypes from 'prop-types';
 
 export default class Product extends Component {
+  renderButtonColor = (remarks) => {
+    if (remarks <= 1) return 'danger';
+    else if (remarks <= 2) return 'primary';
+    else if (remarks <= 3) return 'warning';
+    else if (remarks <= 4) return 'info';
+    else return 'success';
+  };
+
+  renderTags = (tags) => {
+    let items = tags.map((tag, idx) => {
+      return (
+        <button
+          type='button'
+          key={idx}
+          className='btn btn-light mr-2 tag-button'
+        >
+          {tag}
+        </button>
+      );
+    });
+    return items;
+  };
+
   render() {
     const {
       college_name,
+      famous_nearest_places,
       promoted,
       rating,
       rating_remarks,
       tags,
       original_fees,
       discounted_fees,
-      famous_nearest_places,
       amenties,
       discount,
       fees_cycle,
@@ -21,27 +42,57 @@ export default class Product extends Component {
       offertext,
       ranking,
     } = this.props.college;
-    return (
-      <div className='col-md-6'>
-        <ProductConsumer>
-          {(value) => (
-            <div className='card mx-auto my-2 shadow-sm p-3 mb-5 bg-white rounded'>
-              <img
-                src={`img/${image}`}
-                className='card-img-top'
-                alt={college_name}
-              />
-              <br />
+    console.log(this.props.college);
 
-              <div className='card-body p-0'>
-                <h5 className='card-title text-truncate'>{college_name}</h5>
-                <p className='card-text'>Price : </p>
-                <hr />
-                <div></div>
+    return (
+      <div className='col-md-6 p-1'>
+        {promoted && (
+          <p className='text-right'>
+            <span className='prom'>PROMOTED</span>
+          </p>
+        )}
+        <div className='card mx-auto my-2 shadow-sm mb-5 bg-white rounded'>
+          <div
+            className='background card-img-top'
+            style={{
+              backgroundImage: `url(img/${image})`,
+              position: 'relative',
+              height: '300px',
+              width: '100%',
+            }}
+          >
+            <div className='b-layer'>
+              <div className='col-md-12 dflex justifycontentend mt-5'>
+                <button
+                  type='button'
+                  className={`btn btn-${this.renderButtonColor(
+                    rating_remarks
+                  )}`}
+                >
+                  {rating} / 5
+                  <br />
+                  {rating_remarks}
+                </button>
+              </div>
+              <div className='col-md-12 pos-abs pos-b-15'>
+                {this.renderTags(tags)}
+                <button
+                  type='button'
+                  className='btn btn-link text-white fs14 pos-abs pos-r-0'
+                >
+                  #{ranking}
+                </button>
               </div>
             </div>
-          )}
-        </ProductConsumer>
+          </div>
+
+          <div className='card-body p-3'>
+            <h5 className='card-title text-truncate'>{college_name}</h5>
+            <p className='card-text'>
+              {famous_nearest_places} | {}{' '}
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
